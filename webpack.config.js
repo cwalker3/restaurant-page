d2aa5d153824206c9e03e3d7db4427d54ba1abbd
Cory Walker <cory.walker.dev@gmail.com>
2023-06-14 17:02:35 -0700

    add sanitize.css and create style.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
