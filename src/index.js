fcbb21c0b41c6df6afb7b6ed6bd3f8b39e4495c3
Cory Walker <cory.walker.dev@gmail.com>
2023-06-15 14:19:09 -0700

    create modules for content loading

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import 'sanitize.css';
 import './style.scss';
 
 import { loadNav } from './load-nav.js';
+import { loadHome } from './load-home.js';
+import { loadMenu } from './load-menu.js';
+import { loadContact } from './load-contact.js';
 
 window.onload = () => {
   loadNav();
-  //loadHomeContent();
+  loadHome();
 };
